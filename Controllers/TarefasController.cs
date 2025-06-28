b85e044d301794859554b22498a60a0088e9632f
Genildon Barreto <genildonbarreto@yahoo.com.br>
2025-06-27 21:51:29 -0300

    Adicionadas tarefas e método para exibir todas as tarefas na classe TarefasController

--- Controllers/TarefasController.cs
+++ Controllers/TarefasController.cs
@@ -2,11 +2,49 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TaskFlow_API.Models;
 
 namespace TaskFlow_API.Controllers
 {
-    public class TarefasController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TarefasController : ControllerBase
     {
-        
+        private static List<Tarefa> tarefas = new List<Tarefa>
+        {
+            new Tarefa
+            {
+                Id = Guid.NewGuid(),
+                Titulo = "Tarefa 1",
+                Descricao = "Criar a API de Tarefas",
+                Status = "Concluída",
+                DataCriacao = DateTime.Now.AddHours(-1),
+                DataConclusao = DateTime.MinValue
+            },
+            new Tarefa
+            {
+                Id = Guid.NewGuid(),
+                Titulo = "Tarefa 2",
+                Descricao = "Implementar o endpoint de criação",
+                Status = "Em Progresso",
+                DataCriacao = DateTime.Now.AddHours(-1),
+                DataConclusao = DateTime.MinValue
+            },
+            new Tarefa
+            {
+                Id = Guid.NewGuid(),
+                Titulo = "Tarefa 3",
+                Descricao = "Testar a API",
+                Status = "Pendente",
+                DataCriacao = DateTime.Now.AddHours(-1),
+                DataConclusao = DateTime.Now
+            }
+        };   
+        [HttpGet("Exibir Todas as Tarefas")]
+        public IActionResult GetTodasTarefas()
+        {
+            return Ok(tarefas);
+        }
     }
 }
--- obj/Debug/net8.0/TaskFlow_API.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskFlow_API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskFlow_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3c20deecfaf73c07c1281aad1f9afa8b0d8719c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+482095974e4ffda189938dc5e5cd538ae38632ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskFlow_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskFlow_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

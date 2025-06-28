7a7706a2dc11ed3a087366256e69c9255485cd68
Genildon Barreto <genildonbarreto@yahoo.com.br>
2025-06-27 21:45:20 -0300

    Criação da classe Tarefa no modelo

---
+++ Models/Tarefa.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskFlow_API.Models
+{
+    public class Tarefa
+    {
+        
+    }
+}
